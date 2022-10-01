5541ca8379d95f7bdce8e945815ce3548ea0b87f
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2022-10-01 14:20:07 -0700

    added variables for password characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,27 @@
+var specChar = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', 
+'(', ')', '()', '-', '_', '+', '=', '|', '\\', ']', '[', '}', '{', ':', 
+';', '"', "'", '<', '>', ',', '.', '?', '/'];
+
+var numChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+var lowerChar = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'l', 
+'k', 'j', 'h', 'g', 'f', 'd', 's', 'a', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+
+var upperChar = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'L', 'K', 'J',
+'H', 'G', 'F', 'D', 'S', 'A', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
-  var password = generatePassword();
+  var password = generatepassword();
   var passwordText = document.querySelector("#password");
+   
 
   passwordText.value = password;
+  
 
 }
 
