d573a146bc659278edb9b332f4d7faffec7dc223
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2022-10-01 15:44:02 -0700

    added character types and began building a function to evaluate input and build a string based on it

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,64 @@ var lowerChar = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'l',
 
 var upperChar = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'L', 'K', 'J',
 'H', 'G', 'F', 'D', 'S', 'A', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+//  there are four password options to choose from
+// we need 5 functions, one for determining length, and the four options
 
+
+// a function to trigger the application
+function generatePassword(){
+  
+  
+
+  var passwordLength = propmt('How long would you like your password to be');
+  //create if statement to check value given is between 8-128
+  var inclSpecChar = confirm("Would you like to include special characters");
+  var inclNumChar = confirm("Would you like to include numeric characters");
+  var inclUpperChar = confirm("Would you like to include uppercase characters");
+  var inclLowerChar = confirm("Would you like to include lowercase characters");
+  
+  var options ={
+    passwordLength: passwordLength,
+    inclSpecChar: inclSpecChar,
+    inclNumChar: inclNumChar,
+    inclUpperChar: inclUpperChar,
+    inclLowerChar: inclLowerChar
+  }
+
+  var possPasswordChars= [];
+  var password = [];
+
+  function randChar(array){
+    var randomNumber = Math.floor(math.random() * array.length);
+    return array[randomNumber];
+  }
+  
+
+  if(options.inclSpecChar){
+    // generate random spec care
+    // push it to password
+    possPasswordChars = possPasswordChars.concat(specChar);
+     
+    // math.floor and math.random acan be used to make a random selection
+    password.push(randChar(specChar));
+
+   }
+
+   if(options.inclNumChar){
+    possPasswordChars = possPasswordChars.concat(numChar);
+    password.push(randChar(numChar)); 
+   }
+   if(options.inclUpperChar){
+    possPasswordChars = possPasswordChars.concat(upperChar);
+    password.push(randChar(upperChar)); 
+   }
+   if(options.inclLowerChar){
+    possPasswordChars = possPasswordChars.concat(lowerChar);
+    password.push(randChar(lowerChar)); 
+    
+   }
+
+}
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
